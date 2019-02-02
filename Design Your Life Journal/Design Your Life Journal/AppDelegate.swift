189b7a03129bc6084a32fff3894ca1233d07d870
Frulwinn <frulwinn@gmail.com>
2019-02-01 16:51:53 -0800

    initial commit

---
+++ Design Your Life Journal/Design Your Life Journal/AppDelegate.swift
@@ -0,0 +1,17 @@
+//  AppDelegate.swift
+//  Design Your Life Journal
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        return true
+    }
+}
+
