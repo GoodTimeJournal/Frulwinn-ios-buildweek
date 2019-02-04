ab0945c7018dbf4da39073df1ffe3ded39ac666f
Frulwinn <frulwinn@gmail.com>
2019-02-04 10:42:57 -0800

    revised struct to work with database created on trello

--- Design Your Life Journal/Design Your Life Journal/Model/Model.swift
+++ Design Your Life Journal/Design Your Life Journal/Model/Model.swift
@@ -1,3 +1,15 @@
 //  Copyright © 2019 Frulwinn. All rights reserved.
 
 import Foundation
+
+class Model {
+    static let shared = Model()
+    private init () {}
+    
+    //MARK: - Properties
+
+    
+    //MARK: - Methods
+   
+    
+}
---
+++ Design Your Life Journal/Design Your Life Journal/Model/Users.swift
@@ -0,0 +1,27 @@
+//  Copyright © 2019 Frulwinn. All rights reserved.
+
+import Foundation
+
+struct Users: Codable {
+    var id: String
+    var username: String //required 32 char max
+    var password: String //required 32 char max
+    var avatar: String
+    var activity: [Activity]
+    var reflection: [Reflection]
+}
+
+struct Activity: Codable {
+    var timestamp: String
+    var name: String //title
+    var engagement: String //detail
+    var enjoymentRating: Bool = false
+}
+
+struct Reflection: Codable {
+    var timestamp: String
+    var JournalEntry: String // journal entry title?
+    var suprises: String //surprises worked
+    var insights: String // insights did not work
+    
+}
