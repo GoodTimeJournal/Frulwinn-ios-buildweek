ebce33a30945d22f2223a0592681ba374321bc01
Frulwinn <frulwinn@gmail.com>
2019-02-04 20:22:04 -0800

    added reflectionsTVCell updateViews and reflection variable

--- Design Your Life Journal/Design Your Life Journal/Controllers/ReflectionsTVCell.swift
+++ Design Your Life Journal/Design Your Life Journal/Controllers/ReflectionsTVCell.swift
@@ -3,10 +3,26 @@
 import UIKit
 
 class ReflectionsTVCell: UITableViewCell {
+    
+    //MARK: - Properties
+    var reflection: Reflection? {
+        didSet {
+            updateViews()
+        }
+    }
 
     //MARK: - Outlets
     @IBOutlet weak var journalEntryLabel: UILabel!
     @IBOutlet weak var timestampLabel: UILabel!
     
-
+    private func updateViews() {
+        guard let reflection = reflection else { return }
+        journalEntryLabel.text = reflection.journalEntry
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M/d/yy, h:mm a"
+        
+        let timestampFormatted = dateFormatter.string(from: reflection.timestamp)
+        timestampLabel.text = timestampFormatted
+    }
 }
